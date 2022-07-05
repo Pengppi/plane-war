2dc2db7c0489bde4459835f1c45940724b0a7730
pengi <1781287641@qq.com>
2022-07-05 23:42:28 +0800

    添加飞机鼠标移动方式

--- src/com/tedu/controller/GameListener.java
+++ src/com/tedu/controller/GameListener.java
@@ -2,6 +2,8 @@ package com.tedu.controller;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,7 +17,7 @@ import com.tedu.manager.GameElement;
  * @author renjj
  *
  */
-public class GameListener implements KeyListener{
+public class GameListener implements KeyListener, MouseMotionListener {
 	private ElementManager em=ElementManager.getManager();
 	
 	/*能否通过一个集合来记录所有按下的键，如果重复触发，就直接结束
@@ -23,8 +25,7 @@ public class GameListener implements KeyListener{
 	 *       松开就直接删除集合中的记录。
 	 * set集合
 	 * */
-	private Set<Integer> set=new HashSet<Integer>();
-	
+
 	@Override
 	public void keyTyped(KeyEvent e) {
 	}
@@ -34,30 +35,29 @@ public class GameListener implements KeyListener{
 	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
-//		拿到玩家集合
-		System.out.println("按下"+e.getKeyCode());
-		int key=e.getKeyCode();
-		if(set.contains(key)) { //判定集合中是否已经存在,包含这个对象
-//			如果包含直接结束方法
-			return;
-		}
-		set.add(key);
-		List<ElementObj> play = em.getElementsByKey(GameElement.PLAY);
-		for(ElementObj obj:play) {
-			obj.keyClick(true, e.getKeyCode());
-		}
+
 	}
 	/**松开*/
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(!set.contains(e.getKeyCode())) {//如果这个不存在，就停止
-			return;
-		}//存在(已经按过这个案件)
-		set.remove(e.getKeyCode());//移除数据
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+//        System.out.println("dragged");
 		List<ElementObj> play = em.getElementsByKey(GameElement.PLAY);
-		for(ElementObj obj:play) {
-			obj.keyClick(false, e.getKeyCode());
+		for (ElementObj obj : play) {
+			obj.mouseMove(e.getX(), e.getY());
 		}
 	}
 
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		System.out.println(e.getX() + " " + e.getY());
+		List<ElementObj> play = em.getElementsByKey(GameElement.PLAY);
+		for (ElementObj obj : play) {
+			obj.mouseMove(e.getX(), e.getY());
+		}
+	}
 }
